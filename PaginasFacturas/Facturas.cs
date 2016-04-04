d4db902a2b4ac6a3d25a4882cd8bef99ceecfeca
Jennifer <jvs1192@gmail.com>
2016-04-04 17:30:00 +0200

    First Commit

---
+++ PaginasFacturas/Facturas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaginasFacturas
+{
+    public class Facturas
+    {
+        public int IdFactura { get; set; }
+  
+        public IEnumerable<Facturas> GetPage(IEnumerable<Facturas> factura,int paginas,int totalPorPagina ){
+                return factura.Skip((paginas-1)*totalPorPagina).Take(totalPorPagina);
+            }
+    }
+}
---
+++ PaginasFacturas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaginasFacturas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var facturasList = new List<Facturas>() 
+            {
+                new Facturas(){IdFactura=1},
+                new Facturas(){IdFactura=2},
+                new Facturas(){IdFactura=3},
+                new Facturas(){IdFactura=4},
+            };
+
+            var factura = new Facturas();
+            var v=factura.GetPage(facturasList, 1, 2);
+            foreach (var bucle in v) {
+                Console.WriteLine(bucle.IdFactura);
+            }
+            Console.ReadLine();
+        }
+    }
+}
